5daf0382fad8ae35aaf7cdead8b20dff2f61f84b
Harris <harris@user3-203-207.wireless.utoronto.ca>
2015-02-13 17:20:39 -0500

    fixed template

--- communityfund/urls.py
+++ communityfund/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'communityfund.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^/', HomeView.as_view(), name="home"),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HomeView.as_view(), name="home"),
 )
