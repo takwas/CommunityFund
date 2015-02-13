f8a522ac2e640cf0dfa11ba7aa803b5a0594938b
Harris <harris@user3-203-207.wireless.utoronto.ca>
2015-02-13 17:12:17 -0500

    templates

---
+++ home/migrations/0002_funded.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('home', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Funded',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('amount', models.DecimalField(verbose_name=b'Amount', max_digits=19, decimal_places=2)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
