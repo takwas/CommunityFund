3e4ae7b1943a1731934705861806c033da8d7b86
Harris <harris@user3-203-207.wireless.utoronto.ca>
2015-02-13 17:32:40 -0500

    updated model

--- home/models.py
+++ home/models.py
@@ -3,45 +3,41 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Project(models.Model):
-
     initiator = models.ForeignKey(User)
     name = models.CharField("Name", max_length=100)
     description = models.TextField("Description")
     funding_goal = models.DecimalField("Funding Goal", max_digits=19, decimal_places=2)
     current_funds = models.DecimalField("Current Funds", max_digits=19, decimal_places=2)
 
-
 class Funded(models.Model):
     project = models.ForeignKey(Project)
     user = models.ForeignKey(User)
     amount = models.DecimalField("Amount", max_digits=19, decimal_places=2)
 
-
-class Interests(models.Model):
+class UserInterests(models.Model):
+    user = models.ForeignKey(User, name="user_interests")
     interest = models.CharField("Interest", max_length=100)
 
+class CommunityInterests(models.Model):
+    community = models.ForeignKey(Community, related_name="comm_interests")
+    interest = models.CharField("Interest", max_length=100)
 
 class Community(models.Model):
     name = models.CharField("Name", max_length=100)
     location = models.CharField("Location", max_length=100)
-    interest = models.ForeignKey(Interests)
-
 
 class ProjectReputation(models.Model):
-    rated = models.ForeignKey(Project, related_name="Project_Rep")
+    rated = models.ForeignKey(Project, related_name="project_rep")
     rater = models.ForeignKey(User) 
     rating = models.IntegerField("Rating", 
         validators=[MaxValueValidator(5),MinValueValidator(0)])
 
-
 class UserReputation(models.Model):
-    rated = models.ForeignKey(User, related_name="User_Rep")
+    rated = models.ForeignKey(User, related_name="user_rep")
     rater = models.ForeignKey(User) 
     rating = models.IntegerField("Rating", 
         validators=[MaxValueValidator(5),MinValueValidator(0)])
        
-
-
 class Like(models.Model):
     user = models.ForeignKey(User)
     interest = models.ForeignKey(Interests)
