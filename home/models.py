ce0571418e0dce8245ccecd1b099ffe376bee9b5
Harris <harris@user3-203-207.wireless.utoronto.ca>
2015-02-13 16:40:00 -0500

    added project model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+
+    initiator = models.ForeignKey(User)
+    name = models.CharField("Name", max_length=100)
+    description = models.TextField("Description")
+    funding_goal = models.DecimalField("Funding Goal", max_digits=19, decimal_places=2)
+    current_funds = models.DecimalField("Current Funds", max_digits=19, decimal_places=2)
