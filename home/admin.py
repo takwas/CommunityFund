8895a24703da244290f9e6961e72a2a512c2262c
HarrisC <harris2122@gmail.com>
2015-03-11 00:19:17 -0400

    typo in import UserAdmin

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 from .models import *
 
 # Add each model to django admin site so that we can edit them
